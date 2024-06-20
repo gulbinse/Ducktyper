4deef52aeef18270d5028124bf16303d1ae0a1b6
Tobias Daake <tobi.bergwusel@gmx.de>
2024-06-20 10:24:16 +0200

    Added Javadoc

--- game/src/main/java/typeracer/game/GameState.java
+++ game/src/main/java/typeracer/game/GameState.java
@@ -24,6 +24,11 @@ public class GameState {
     private final Map<String, Player> players;
     private final Map<Player, Integer> progress;
 
+    /**
+     * A constructor which creates a new default gameState.
+     *
+     * @param textSource from which the Texts of the Game should come from
+     */
     GameState(TextSource textSource) {
         this(textSource, "", GameStatus.WAITINGFORPLAYERS, Collections.emptyMap(), Collections.emptyMap());
     }
@@ -44,6 +49,13 @@ public class GameState {
         this.gameStatus = gameStatus;
     }
 
+    /**
+     * Adds a Player to the game.
+     *
+     * <p>Not implemented yet a check if there are already maximum number of players in the game</p>
+     * @param player that is added to the game
+     * @return the new GameState
+     */
     public synchronized GameState addPlayer(Player player) {
         String playerName = player.getName();
         Map<String, Player> updatedPlayers = new HashMap<>(players);
@@ -53,6 +65,12 @@ public class GameState {
         return new GameState(textSource, currentText, gameStatus, updatedPlayers, updatedProgress);
     }
 
+    /**
+     * Removes a Player from the game.
+     *
+     * @param player that is removed from the game
+     * @return the new GameState
+     */
     public synchronized GameState removePlayer(Player player) {
         String playerName = player.getName();
         Map<String, Player> updatedPlayers = new HashMap<>(players);
@@ -60,22 +78,51 @@ public class GameState {
         return new GameState(textSource, currentText, gameStatus, updatedPlayers, progress);
     }
 
+    /**
+     * Starts a new round.
+     *
+     * @param newText which shall be copied in the next round
+     * @return new GameState with new text
+     */
+    //Man könnte auch in TextSource eine Methode implementieren, die den nächsten Text zurück gibt.
+    //Dann könnte man statt dem Parameter new Text GameState mit newGamestate(..., textSource.getNextText, ...) initialisieren
     public synchronized GameState nextRound(String newText) {
         return new GameState(textSource, newText, gameStatus, players, progress);
     }
 
+    /**
+     * Returns List of players in the current game.
+     *
+     * @return List of players
+     */
     public List<Player> getPlayers() {
         return List.copyOf(players.values());
     }
 
+    /**
+     * Returns Map with Player and their corresponding progress.
+     *
+     * @return Map with progress.
+     */
     public Map<Player, Integer> getProgress() {
         return Map.copyOf(progress);
     }
 
+    /**
+     * Returns the TextSource of the current GameState.
+     *
+     * @return TextSource
+     */
     public TextSource getTextSource() {
         return textSource;
     }
 
+    /**
+     * Returns the Status of the current game.
+     *
+     * <p>Possible Status is: ONGOING, FINISHED, WAITINGFORPLAYERS </p>
+     * @return currrent Status
+     */
     public GameStatus getStatus() {
         return gameStatus;
     }
