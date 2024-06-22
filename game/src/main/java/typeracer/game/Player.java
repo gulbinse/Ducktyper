ed6a0b0be1c045a4bef571272954a6b5bf831ea0
Eric Gulbins <gulbinse@cip.ifi.lmu.de>
2024-06-22 20:31:08 +0200

    Add typeLetter, WPM & Progress methods
    Allows players to type letters, which get forwarded from the game to the specified player, who checks the letter, and updates WPM and progress if necessary. Returns a TypingState stating whether the typing was successful or not.

--- game/src/main/java/typeracer/game/Player.java
+++ game/src/main/java/typeracer/game/Player.java
@@ -3,7 +3,12 @@ package typeracer.game;
 /** Represents a player of the game. */
 public class Player {
   private String username;
-  private PlayerState state;
+  private final PlayerState state;
+
+  public enum TypingResult {
+    SUCCESSFUL,
+    UNSUCCESSFUL
+  }
 
   public Player(String username) {
     this.username = username;
@@ -30,7 +35,37 @@ public class Player {
     return state.isReady();
   }
 
-  public void typeLetter(char letter) {
+  public TypingResult typeLetter(char typedLetter, String textToType, long gameStartTime) {
+    int currentTextIndex = state.getCurrentTextIndex();
+    char correctLetter = textToType.charAt(currentTextIndex);
 
+    double progress = (double) currentTextIndex / textToType.length();
+    assert 0 <= progress && progress <= 1;
+
+    // Update WPM in every case, since it might change with a wrong letter as well
+    updateWPM(gameStartTime);
+
+    if (typedLetter == correctLetter) {
+      state.setCurrentTextIndex(currentTextIndex + 1);
+
+      // Update Progress only if typing was successful, to avoid unnecessary updates
+      state.setProgress(progress);
+
+      if (correctLetter == ' ') {
+        state.incrementNumTypedWords();
+      }
+      return TypingResult.SUCCESSFUL;
+    }
+    return TypingResult.UNSUCCESSFUL;
+  }
+
+  private void updateWPM(long gameStartTime) {
+    long elapsedTime = System.nanoTime() - gameStartTime;
+    double elapsedMinutes = (double) elapsedTime / (10^9);
+    double wpm = state.getNumTypedWords() / elapsedMinutes;
+
+    assert wpm >= 0;
+
+    state.setWPM(wpm);
   }
 }
