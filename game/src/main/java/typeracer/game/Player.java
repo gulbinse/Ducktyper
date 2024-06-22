e820b97c518b9d1a66bf29574918382126e605b8
Eric Gulbins <gulbinse@cip.ifi.lmu.de>
2024-06-22 16:18:53 +0200

    Add isReady() to Player

--- game/src/main/java/typeracer/game/Player.java
+++ game/src/main/java/typeracer/game/Player.java
@@ -1,118 +1,41 @@
 package typeracer.game;
 
+import typeracer.game.observable.Observable;
+import typeracer.game.observable.Observer;
+
 /** Represents a player of the game. */
-public class Player {
+public class Player implements Observer {
   private String username;
-  private final PlayerState state;
+  private PlayerState state;
 
-  /** A result of trying to type a letter. */
-  public enum TypingResult {
-    /** The typing was successful. */
-    SUCCESSFUL,
-
-    /** The typing was unsuccessful. */
-    UNSUCCESSFUL
-  }
-
-  /**
-   * Creates a new Player with the given username.
-   *
-   * @param username The username of the player
-   */
   public Player(String username) {
     this.username = username;
     state = new PlayerState();
   }
 
-  /**
-   * Returns this Player's username.
-   *
-   * @return this Player's username
-   */
-  public String getUsername() {
+  @Override
+  public void update(Observable o, Object arg) {}
+
+  @Override
+  public void update() {}
+
+  public String getName() {
     return username;
   }
 
-  /**
-   * Returns this Player's current progress.
-   *
-   * @return this Player's current progress
-   */
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
   public double getProgress() {
     return state.getProgress();
   }
 
-  /**
-   * Returns this Player's current words per minute.
-   *
-   * @return this Player's current words per minute
-   */
-  public double getWordsPerMinute() {
-    return state.getWordsPerMinute();
+  public double getWPM() {
+    return state.getWPM();
   }
 
-  /**
-   * Returns true if this Player is ready, false otherwise.
-   *
-   * @return true if this Player is ready, false otherwise
-   */
-  public boolean isReady() {
-    return state.isReady();
-  }
+  public void typeLetter(char letter) {
 
-  /**
-   * Makes this player type the given letter. Checks if the typed letter appears in the given text
-   * at the position this Player is currently at, and calculates updated words per minute and
-   * progress if necessary.
-   *
-   * @param typedLetter the letter this player has typed
-   * @param textToType the text the player has to type
-   * @param gameStartTime the time the game started at, given as a long like returned by
-   *     System.nanoTime()
-   * @return {@link TypingResult#SUCCESSFUL} if the letter was correct, else {@link
-   *     TypingResult#UNSUCCESSFUL}
-   */
-  public synchronized TypingResult typeLetter(
-      char typedLetter, String textToType, long gameStartTime) {
-    int currentTextIndex = state.getCurrentTextIndex();
-    char correctLetter = textToType.charAt(currentTextIndex);
-
-    double progress = (double) currentTextIndex / textToType.length();
-    assert 0 <= progress && progress <= 1;
-
-    // Update words per minute in every case, since it might change with a wrong letter as well
-    updateWordsPerMinute(gameStartTime);
-
-    if (typedLetter == correctLetter) {
-      state.setCurrentTextIndex(currentTextIndex + 1);
-
-      // Update Progress only if typing was successful, to avoid unnecessary updates
-      state.setProgress(progress);
-
-      if (Character.isSpaceChar(correctLetter)) {
-        state.incrementNumTypedWords();
-      }
-      return TypingResult.SUCCESSFUL;
-    }
-    return TypingResult.UNSUCCESSFUL;
-  }
-
-  private synchronized void updateWordsPerMinute(long gameStartTime) {
-    long elapsedTime = System.nanoTime() - gameStartTime;
-    double elapsedMinutes = (double) elapsedTime / (10 ^ 9);
-    double wordsPerMinute = state.getNumTypedWords() / elapsedMinutes;
-
-    assert wordsPerMinute >= 0;
-
-    state.setWordsPerMinute(wordsPerMinute);
-  }
-
-  /**
-   * Sets the ready state for this player.
-   *
-   * @param isReady true if player is ready, false otherwise
-   */
-  public void setIsReady(boolean isReady) {
-    state.setIsReady(isReady);
   }
 }
--- game/src/main/java/typeracer/game/PlayerState.java
+++ game/src/main/java/typeracer/game/PlayerState.java
@@ -4,99 +4,23 @@ package typeracer.game;
 public class PlayerState {
   private double progress = 0; // Ranging from 0.0 to 1.0 according to the protocol
   private double wordsPerMinute = 0;
-  private boolean isReady = false;
-  private int currentTextIndex = 0;
-  private int numTypedWords = 0;
 
   /** The default constructor of this class. */
   public PlayerState() {}
 
-  /**
-   * Returns the current progress.
-   *
-   * @return the current progress
-   */
   public double getProgress() {
     return progress;
   }
 
-  /**
-   * Sets the current progress to the specified value.
-   *
-   * @param progress the new progress
-   */
   public void setProgress(double progress) {
     this.progress = progress;
   }
 
-  /**
-   * Returns the current speed in words per minute.
-   *
-   * @return the current speed in words per minute
-   */
-  public double getWordsPerMinute() {
+  public double getWPM() {
     return wordsPerMinute;
   }
 
-  /**
-   * Sets the words per minute to the specified value.
-   *
-   * @param wordsPerMinute the new words per minute
-   */
-  public void setWordsPerMinute(double wordsPerMinute) {
-    this.wordsPerMinute = wordsPerMinute;
-  }
-
-  /**
-   * Returns true if the Player is ready, false otherwise.
-   *
-   * @return true if the Player is ready, false otherwise
-   */
-  public boolean isReady() {
-    return isReady;
-  }
-
-  /**
-   * Sets the Player to being ready if true is given, to not ready if false is given.
-   *
-   * @param isReady the readiness state to set this Player to
-   */
-  public void setIsReady(boolean isReady) {
-    this.isReady = isReady;
-  }
-
-  /**
-   * Returns the zero-indexed position in the current text as an integer.
-   *
-   * @return the current text index
-   */
-  public int getCurrentTextIndex() {
-    return currentTextIndex;
-  }
-
-  /**
-   * Sets the zero-indexed position in the current text to the specified integer.
-   *
-   * @param currentTextIndex the index to set
-   */
-  public void setCurrentTextIndex(int currentTextIndex) {
-    this.currentTextIndex = currentTextIndex;
-  }
-
-  /**
-   * Returns the number of typed words until now.
-   *
-   * @return the number of typed words until now
-   */
-  public int getNumTypedWords() {
-    return numTypedWords;
-  }
-
-  /**
-   * Increases the number of typed words by one. This should generally be the only way the player's
-   * number of typed words is changed.
-   */
-  public synchronized void incrementNumTypedWords() {
-    numTypedWords++;
+  public void setWPM(double wpm) {
+    this.wordsPerMinute = wpm;
   }
 }
