81934b4d41c2f39d105f0c625494865132ed5bcb
Eric Gulbins <gulbinse@cip.ifi.lmu.de>
2024-06-22 15:39:21 +0200

    Add basic implementation of TextSource
    Add methods to set the current text from a certain file, along with a method for getting a default text.

--- game/src/main/java/typeracer/game/TextSource.java
+++ game/src/main/java/typeracer/game/TextSource.java
@@ -1,8 +1,38 @@
 package typeracer.game;
 
+import java.io.*;
+
 /** Provides a text to use as prompt for the game. */
 public class TextSource {
+  private final String TEXT_SOURCE_FOLDER = "text_sources";
+  private final String DEFAULT_TEXT_FILE = "bee_movie.txt";
+
+  private String currentText;
 
   /** The default constructor of this class. */
   public TextSource() {}
+
+  @Deprecated
+  public String setDefaultText() throws IOException {
+    currentText = getTextFromFile(new File(TEXT_SOURCE_FOLDER, DEFAULT_TEXT_FILE));
+    return currentText;
+  }
+
+  public String getTextFromFile(File file) throws IOException {
+    try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+      String line = reader.readLine();
+
+      StringBuilder textBuilder = new StringBuilder();
+        while (line != null) {
+          textBuilder.append(line).append("\n");
+          line = reader.readLine();
+        }
+        currentText = textBuilder.toString();
+        return currentText;
+      }
+  }
+
+  public String getCurrentText() {
+    return currentText;
+  }
 }
