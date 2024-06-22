45d153b7e3da09dc9d0871f7c1e8733023b7fe8b
Adrian Moritz <ad.moritz@campus.lmu.de>
2024-06-22 02:12:35 +0200

    Refactor message handling for better compatibility

---
+++ server/src/main/java/typeracer/server/message/MessageHandler.java
@@ -0,0 +1,45 @@
+package typeracer.server.message;
+
+import typeracer.communication.messages.Message;
+import typeracer.server.Lobby;
+import typeracer.server.message.handlers.CharacterRequestHandler;
+import typeracer.server.message.handlers.JoinGameRequestHandler;
+import typeracer.server.message.handlers.ReadyRequestHandler;
+
+/** Abstract class for handling messages in a chain of responsibility pattern. */
+public abstract class MessageHandler {
+
+  /**
+   * Handles the incoming message. Implementations should provide specific handling logic.
+   *
+   * @param message the message to handle
+   * @param lobby the lobby this handler belongs to
+   * @param id the unique id of the player
+   */
+  public abstract void handleMessage(Message message, Lobby lobby, int id);
+
+  /**
+   * Sets the next handler in the chain.
+   *
+   * @param handler the next handler in the chain
+   * @return a new MessageHandler instance with the set handler
+   */
+  public abstract MessageHandler setNext(MessageHandler handler);
+
+  /**
+   * Creates a chain of responsibility for handling messages. The chain consists of the following
+   * handlers in order:
+   *
+   * <ul>
+   *   <li>{@link CharacterRequestHandler}
+   *   <li>{@link JoinGameRequestHandler}
+   *   <li>{@link ReadyRequestHandler}
+   * </ul>
+   *
+   * @return the first MessageHandler in the created chain
+   */
+  public static MessageHandler createChain() {
+    return new CharacterRequestHandler()
+        .setNext(new JoinGameRequestHandler().setNext(new ReadyRequestHandler()));
+  }
+}
---
+++ server/src/main/java/typeracer/server/message/handlers/ReadyRequestHandler.java
@@ -0,0 +1,31 @@
+package typeracer.server.message.handlers;
+
+import typeracer.communication.messages.Message;
+import typeracer.server.Lobby;
+import typeracer.server.message.MessageHandler;
+
+/**
+ * Handles ReadyRequest messages in a chain of responsibility pattern. If the message is not of the
+ * specified type, it will be passed to the next handler in the chain, if any.
+ */
+public class ReadyRequestHandler extends MessageHandler {
+
+  private final MessageHandler nextHandler;
+
+  /** The default constructor of this class. */
+  public ReadyRequestHandler() {
+    this.nextHandler = null;
+  }
+
+  private ReadyRequestHandler(MessageHandler nextHandler) {
+    this.nextHandler = nextHandler;
+  }
+
+  @Override
+  public void handleMessage(Message message, Lobby lobby, int id) {}
+
+  @Override
+  public ReadyRequestHandler setNext(MessageHandler handler) {
+    return new ReadyRequestHandler(handler);
+  }
+}
