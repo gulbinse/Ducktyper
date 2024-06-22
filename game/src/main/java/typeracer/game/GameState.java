3a33f3adba693018a9f9abe6f989b5a0e110939a
Eric Gulbins <gulbinse@cip.ifi.lmu.de>
2024-06-22 16:14:48 +0200

    Remove unnecessary objects from GameState
    currentText has been removed as it can be returned by the textSource.
    Also, the creation of new GameState objects at runtime has been replaced by setting the variables directly.

--- game/src/main/java/typeracer/game/GameState.java
+++ game/src/main/java/typeracer/game/GameState.java
@@ -1,7 +1,6 @@
 package typeracer.game;
 
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -14,19 +13,18 @@ public class GameState {
     /** Represents the Status of the TypeWriter game. */
     public enum GameStatus {
         /** Game is still running. */
-        ONGOING,
+        RUNNING,
         /** Game is over because all players have finished their text. */
         FINISHED,
         /** Game hasn't started yet because not every player is ready yet. */
         WAITING_FOR_READY
     }
 
-    private final GameStatus gameStatus;
+    private GameStatus gameStatus;
 
     private final TextSource textSource;
-    private final String currentText;
 
-    private final Map<String, Player> players;
+    private Map<String, Player> players;
 
     /**
      * A constructor which creates a new default gameState.
@@ -35,24 +33,21 @@ public class GameState {
      * @param textSource from which the Texts of the Game should come from
      */
     GameState(TextSource textSource) {
-        this(textSource, "", GameStatus.WAITING_FOR_READY, Collections.emptyMap());
+        this(textSource, GameStatus.WAITING_FOR_READY, Collections.emptyMap());
     }
 
     /**
      * Private Constructor for GameState class.
      *
      * @param textSource for the Game
-     * @param newText which shall be copied
      * @param gameStatus of the Game
      * @param players in the Lobby
      */
     private GameState(
             TextSource textSource,
-            String newText,
             GameStatus gameStatus,
             Map<String, Player> players) {
         this.textSource = textSource;
-        currentText = newText;
         this.players = Map.copyOf(players);
         this.gameStatus = gameStatus;
     }
@@ -62,38 +57,20 @@ public class GameState {
      *
      * <p>Not implemented yet a check if there are already maximum number of players in the game</p>
      * @param player that is added to the game
-     * @return the new GameState
      */
-    public synchronized GameState addPlayer(Player player) {
+    public synchronized void addPlayer(Player player) {
         String playerName = player.getName();
-        Map<String, Player> updatedPlayers = new HashMap<>(players);
-        updatedPlayers.put(playerName, player);
-        return new GameState(textSource, currentText, GameStatus.WAITING_FOR_READY, updatedPlayers);
+        players.put(playerName, player);
     }
 
     /**
      * Removes a Player from the game.
      *
      * @param player that is removed from the game
-     * @return the new GameState
      */
-    public synchronized GameState removePlayer(Player player) {
+    public synchronized void removePlayer(Player player) {
         String playerName = player.getName();
-        Map<String, Player> updatedPlayers = new HashMap<>(players);
-        updatedPlayers.remove(playerName);
-        return new GameState(textSource, currentText, gameStatus, updatedPlayers);
-    }
-
-    /**
-     * Starts a new round.
-     *
-     * @param newText which shall be copied in the next round
-     * @return new GameState with new text
-     */
-    //Man könnte auch in TextSource eine Methode implementieren, die den nächsten Text zurück gibt.
-    //Dann könnte man statt dem Parameter new Text GameState mit newGamestate(..., textSource.getNextText, ...) initialisieren
-    public synchronized GameState nextRound(String newText) {
-        return new GameState(textSource, newText, gameStatus, players);
+        players.remove(playerName);
     }
 
     /**
@@ -117,10 +94,14 @@ public class GameState {
     /**
      * Returns the Status of the current game.
      *
-     * <p>Possible Status is: {@link GameStatus#ONGOING}, {@link GameStatus#FINISHED}, {@link GameStatus#WAITING_FOR_READY} </p>
+     * <p>Possible Status is: {@link GameStatus#RUNNING}, {@link GameStatus#FINISHED}, {@link GameStatus#WAITING_FOR_READY} </p>
      * @return current Status
      */
     public GameStatus getStatus() {
         return gameStatus;
+    }
+
+    public void setStatus(GameStatus status) {
+        gameStatus = status;
     }
 }
