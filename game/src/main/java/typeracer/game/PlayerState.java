8c688650004a1eb47b77253c693178d36954db64
Eric Gulbins <gulbinse@cip.ifi.lmu.de>
2024-06-22 14:24:38 +0200

    Add basic implementation of PlayerState

--- game/src/main/java/typeracer/game/PlayerState.java
+++ game/src/main/java/typeracer/game/PlayerState.java
@@ -2,7 +2,25 @@ package typeracer.game;
 
 /** Manages the state of a single player. */
 public class PlayerState {
+  private double progress = 0; // Ranging from 0.0 to 1.0 according to the protocol
+  private double wordsPerMinute = 0;
 
   /** The default constructor of this class. */
   public PlayerState() {}
+
+  public double getProgress() {
+    return progress;
+  }
+
+  public void setProgress(double progress) {
+    this.progress = progress;
+  }
+
+  public double getWPM() {
+    return wordsPerMinute;
+  }
+
+  public void setWPM(double wpm) {
+    this.wordsPerMinute = wpm;
+  }
 }
