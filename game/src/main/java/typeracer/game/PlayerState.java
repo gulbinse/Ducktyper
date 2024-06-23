6cf7d2427370fb45387c7e7c4f00d82df6ede9d7
Eric Gulbins <gulbinse@cip.ifi.lmu.de>
2024-06-23 16:35:16 +0200

    Add missing Javadocs & increase maintainability
    Javadocs for getters and setters were omitted. Standardizes method & variable names.

--- game/src/main/java/typeracer/game/PlayerState.java
+++ game/src/main/java/typeracer/game/PlayerState.java
@@ -5,6 +5,8 @@ public class PlayerState {
   private double progress = 0; // Ranging from 0.0 to 1.0 according to the protocol
   private double wordsPerMinute = 0;
   private boolean isReady = false;
+  private int currentTextIndex = 0;
+  private int numTypedWords = 0;
 
   /** The default constructor of this class. */
   public PlayerState() {}
@@ -31,5 +33,21 @@ public class PlayerState {
 
   public void setReady(boolean ready) {
     isReady = ready;
+  }
+
+  public int getCurrentTextIndex() {
+    return currentTextIndex;
+  }
+
+  public void setCurrentTextIndex(int currentTextIndex) {
+    this.currentTextIndex = currentTextIndex;
+  }
+
+  public int getNumTypedWords() {
+    return numTypedWords;
+  }
+
+  public void incrementNumTypedWords() {
+    numTypedWords++;
   }
 }
--- game/src/main/java/typeracer/game/TextSource.java
+++ game/src/main/java/typeracer/game/TextSource.java
@@ -1,8 +1,56 @@
 package typeracer.game;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
 /** Provides a text to use as prompt for the game. */
 public class TextSource {
+  private static final String TEXT_SOURCE_FOLDER = "text_sources";
+  private static final String DEFAULT_TEXT_FILE = "bee_movie.txt";
+
+  private String currentText;
 
   /** The default constructor of this class. */
   public TextSource() {}
+
+  /**
+   * Returns a default text to test the game.
+   *
+   * @return the default text, which is the beginning of the bee movie script
+   * @throws IOException when exceptions with reading the file occur
+   * @deprecated will be replaced by a method returning a randomly selected text
+   */
+  @Deprecated
+  public String setDefaultText() throws IOException {
+    currentText = getTextFromFile(new File(TEXT_SOURCE_FOLDER, DEFAULT_TEXT_FILE));
+    return currentText;
+  }
+
+  /**
+   * Returns the text in the given file.
+   *
+   * @param file the file to read
+   * @return the text in the given file
+   * @throws IOException when exceptions with reading the file occur
+   */
+  public String getTextFromFile(File file) throws IOException {
+    try (BufferedReader reader = new BufferedReader(new FileReader(file, StandardCharsets.UTF_8))) {
+      String line = reader.readLine();
+
+      StringBuilder textBuilder = new StringBuilder();
+      while (line != null) {
+        textBuilder.append(line).append("\n");
+        line = reader.readLine();
+      }
+      currentText = textBuilder.toString();
+      return currentText;
+    }
+  }
+
+  public String getCurrentText() {
+    return currentText;
+  }
 }
