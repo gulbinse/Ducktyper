e57f6762b38d83647d2f056e9cfa6b3c216de68f
Eric Gulbins <gulbinse@cip.ifi.lmu.de>
2024-06-23 18:09:15 +0200

    Add support for IDs
    Allows the Server to address Players by ID instead of username, allowing multiple players with the same username. Game now requires IDs to add or remove Players or to type. GameState manages the mapping of IDs to Players.

--- game/src/main/java/typeracer/game/TypeRacerGame.java
+++ game/src/main/java/typeracer/game/TypeRacerGame.java
@@ -32,16 +32,20 @@ public class TypeRacerGame {
   /**
    * Adds a player to the game.
    *
+   * @param id of the player
    * @param player that will be added to the game
    */
-  public void addPlayer(Player player) {
+  public void addPlayer(int id, Player player) {
     if (!isValidUsername(player.getUsername())) {
       throw new AssertionError(
           "An invalid player name reached the game logic. This should be handled before. Name: "
               + player.getUsername());
     }
     synchronized (this) {
-      state.addPlayer(player);
+      if (state.getIds().contains(id)) {
+        throw new AssertionError("ID " + id + " already contained in player list.");
+      }
+      state.addPlayer(id, player);
       // TODO: notify Mediator about added Player
     }
   }
@@ -61,11 +65,11 @@ public class TypeRacerGame {
   /**
    * Removes a player from the game.
    *
-   * @param player that will be removed
+   * @param id of the player that will be removed
    */
-  public void removePlayer(Player player) {
+  public void removePlayer(int id) {
     synchronized (this) {
-      state.removePlayer(player);
+      state.removePlayer(id);
       // TODO: notify Mediator about removed Player
     }
   }
@@ -73,13 +77,14 @@ public class TypeRacerGame {
   /**
    * Makes the given Player type the given letter.
    *
-   * @param player the player that types
+   * @param id of the player that types
    * @param letter the letter that is typed
    * @return The result of the typing attempt
    */
-  public Player.TypingResult typeLetter(Player player, char letter) {
+  public Player.TypingResult typeLetter(
+      int id, char letter) { // TODO: Does this have to be synchronized?
     // TODO: Check if all players have progress of 1 (i.e. finished the game -> set status to
     // FINISHED)
-    return player.typeLetter(letter, state.getTextToType(), gameStartTime);
+    return state.getPlayerById(id).typeLetter(letter, state.getTextToType(), gameStartTime);
   }
 }
