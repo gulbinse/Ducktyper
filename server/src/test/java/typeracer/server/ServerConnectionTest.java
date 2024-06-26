80b752b9982a3605fc18e6747e5c8c7c9c469d7e
Adrian Moritz <ad.moritz@campus.lmu.de>
2024-06-26 03:29:53 +0200

    Complete server response tests

--- server/src/test/java/typeracer/server/ServerConnectionTest.java
+++ server/src/test/java/typeracer/server/ServerConnectionTest.java
@@ -26,10 +26,10 @@ public class ServerConnectionTest {
   }
 
   @Test
-  public void testServer_receivesJoinRequest_sendsPlayerJoin()
+  public void testServer_receivesJoinRequest_sendsResponseAndPlayerJoin()
       throws IOException, InterruptedException {
-    MockInputStream networkIn =
-        getNetworkIn("{\"messageType\":\"JoinGameRequest\",\"playerName\":\"" + USER1 + "\"}");
+    String joinRequest = "{\"messageType\":\"JoinGameRequest\",\"playerName\":\"" + USER1 + "\"}";
+    MockInputStream networkIn = getNetworkIn(joinRequest);
     ByteArrayOutputStream networkOut = getNetworkOut();
     MockSocket mockSocket = new MockSocket(networkIn, networkOut);
     MockServerSocket serverSocket = new MockServerSocket(List.of(mockSocket));
@@ -42,14 +42,67 @@ public class ServerConnectionTest {
 
     String sent = networkOut.toString(StandardCharsets.UTF_8);
     String[] jsonMessages = sent.split(System.lineSeparator());
+    boolean joinGameResponse = false;
+    boolean playerJoinedNotification = false;
     for (String message : jsonMessages) {
-      if (message.matches(".*\"messageType\":\"PlayerJoinedNotification\".*")
-          && message.matches(".*\"playerName\":\"" + USER1 + "\".*")) {
+      if (message.matches(".*\"messageType\":\"JoinGameResponse\".*")) {
+        assertThat(message.matches(".*\"joinStatus\":\"(ACCEPTED|DENIED)\".*"));
+        assertThat(message.matches(".*\"reason\":\".*\"|null.*"));
+        assertThatContainsNKeyValuePairs(message, 3);
+        joinGameResponse = true;
+      }
+      if (message.matches(".*\"messageType\":\"PlayerJoinedNotification\".*")) {
+        assertThat(message.matches(".*\"playerName\":\"" + USER1 + "\".*"));
         assertThat(message.matches(".*\"numPlayers\":1.*"));
         assertThatContainsNKeyValuePairs(message, 3);
+        playerJoinedNotification = true;
+      }
+      if (joinGameResponse && playerJoinedNotification) {
         return;
       }
     }
-    Assertions.fail("No PlayerJoinedNotification for player " + USER1);
+    Assertions.fail("Missing response for player " + USER1 + " on join request");
+  }
+
+  @Test
+  public void testServer_receivesCharacterRequest_sendsResponseAndPlayerState()
+      throws IOException, InterruptedException {
+    String joinRequest = "{\"messageType\":\"JoinGameRequest\",\"playerName\":\"" + USER1 + "\"}";
+    String characterRequest = "{\"messageType\":\"CharacterRequest\",\"character\":\"a\"}";
+    MockInputStream networkIn =
+        getNetworkIn(joinRequest + System.lineSeparator() + characterRequest);
+    ByteArrayOutputStream networkOut = getNetworkOut();
+    MockSocket mockSocket = new MockSocket(networkIn, networkOut);
+    MockServerSocket serverSocket = new MockServerSocket(List.of(mockSocket));
+
+    TestUtils.startServer(serverSocket);
+    do {
+      Thread.sleep(10);
+    } while (!networkIn.isDone());
+    Thread.sleep(Sleep.BEFORE_TESTING.getMillis());
+
+    String sent = networkOut.toString(StandardCharsets.UTF_8);
+    String[] jsonMessages = sent.split(System.lineSeparator());
+    boolean characterResponse = false;
+    boolean playerState = false;
+    for (String message : jsonMessages) {
+      if (message.matches(".*\"messageType\":\"CharacterResponse\".*")) {
+        assertThat(message.matches(".*\"characterStatus\":(true|false).*"));
+        assertThatContainsNKeyValuePairs(message, 2);
+        characterResponse = true;
+      }
+      if (message.matches(".*\"messageType\":\"PlayerStateNotification\".*")) {
+        assertThat(message.matches(".*\"accuracy\":(0\\.[0-9]+|1\\.0).*"));
+        assertThat(message.matches(".*\"playerName\":\"" + USER1 + "\".*"));
+        assertThat(message.matches(".*\"progress\":(0\\.[0-9]+|1\\.0).*"));
+        assertThat(message.matches(".*\"wpm\":(0|[1-9][0-9]*).*"));
+        assertThatContainsNKeyValuePairs(message, 5);
+        playerState = true;
+      }
+      if (characterResponse && playerState) {
+        return;
+      }
+    }
+    Assertions.fail("Missing response for player " + USER1 + " on character request");
   }
 }
