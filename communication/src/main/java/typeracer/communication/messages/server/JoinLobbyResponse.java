4c256f9b099d1e9b423552c491e047f634d3f005
Adrian Moritz <ad.moritz@campus.lmu.de>
2024-06-28 18:57:55 +0200

    Complete message classes with javadoc and add message converter

--- communication/src/main/java/typeracer/communication/messages/server/JoinLobbyResponse.java
+++ communication/src/main/java/typeracer/communication/messages/server/JoinLobbyResponse.java
@@ -3,23 +3,23 @@ package typeracer.communication.messages.server;
 import typeracer.communication.messages.Message;
 
 /**
- * Response indicating whether a client is allowed to join the game. This response is sent to a
- * specific client after receiving a {@link typeracer.communication.messages.client.JoinGameRequest}
- * from that client. It informs the client whether joining the game is allowed and provides a reason
- * if the request is denied.
+ * Response indicating whether a client is allowed to join the lobby. This response is sent to a
+ * specific client after receiving a {@link
+ * typeracer.communication.messages.client.JoinLobbyRequest} from that client. It informs the client
+ * whether joining the lobby is allowed and provides a reason if the request is denied.
  */
-public final class JoinGameResponse implements Message {
+public final class JoinLobbyResponse implements Message {
 
   private final String joinStatus;
   private final String reason;
 
   /**
-   * Constructs a new JoinGameResponse with the specified arguments.
+   * Constructs a new JoinLobbyResponse with the specified arguments.
    *
    * @param joinStatus the status of the join request, indicating whether joining is allowed
    * @param reason the reason for a denied connection, null otherwise
    */
-  public JoinGameResponse(String joinStatus, String reason) {
+  public JoinLobbyResponse(String joinStatus, String reason) {
     this.joinStatus = joinStatus;
     this.reason = reason;
   }
