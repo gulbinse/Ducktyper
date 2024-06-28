cfe343997d5686789f4d9e03a5da31eef371b970
Adrian Moritz <ad.moritz@campus.lmu.de>
2024-06-28 18:57:55 +0200

    Complete message classes with javadoc and add message converter

--- communication/src/main/java/typeracer/communication/messages/server/CharacterResponse.java
+++ communication/src/main/java/typeracer/communication/messages/server/CharacterResponse.java
@@ -2,14 +2,30 @@ package typeracer.communication.messages.server;
 
 import typeracer.communication.messages.Message;
 
+/**
+ * Response indicating the correctness of a player's typed character. This response is sent from the
+ * server to a specific client after receiving a {@link
+ * typeracer.communication.messages.client.CharacterRequest} from that client. It informs the client
+ * whether the typed character is correct.
+ */
 public final class CharacterResponse implements Message {
 
   private final boolean correct;
 
+  /**
+   * Constructs a new CharacterResponse with the specified arguments.
+   *
+   * @param correct <code>true</code> if the character is correct, <code>false</code> otherwise
+   */
   public CharacterResponse(boolean correct) {
     this.correct = correct;
   }
 
+  /**
+   * Returns whether the typed character is correct.
+   *
+   * @return <code>true</code> if the character is correct, <code>false</code> otherwise
+   */
   public boolean isCorrect() {
     return correct;
   }
--- communication/src/main/java/typeracer/communication/messages/server/PlayerJoinedNotification.java
+++ communication/src/main/java/typeracer/communication/messages/server/PlayerJoinedNotification.java
@@ -2,26 +2,52 @@ package typeracer.communication.messages.server;
 
 import typeracer.communication.messages.Message;
 
+/**
+ * Notification indicating that a new player has joined the game. This notification is sent from the
+ * server to every client to inform them about the new player's arrival.
+ */
 public final class PlayerJoinedNotification implements Message {
 
   private final int numPlayers;
   private final int playerId;
   private final String playerName;
 
+  /**
+   * Constructs a new PlayerJoinedNotification with the specified arguments.
+   *
+   * @param numPlayers the current number of players in the game
+   * @param playerId the id of the joined player
+   * @param playerName the name of the joined player
+   */
   public PlayerJoinedNotification(int numPlayers, int playerId, String playerName) {
     this.numPlayers = numPlayers;
     this.playerId = playerId;
     this.playerName = playerName;
   }
 
+  /**
+   * Returns the current number of players in the game.
+   *
+   * @return the number of players in the game
+   */
   public int getNumPlayers() {
     return numPlayers;
   }
 
+  /**
+   * Returns the id of the joined player.
+   *
+   * @return the player's id
+   */
   public int getPlayerId() {
     return playerId;
   }
 
+  /**
+   * Returns the name of the joined player.
+   *
+   * @return the player's name
+   */
   public String getPlayerName() {
     return playerName;
   }
