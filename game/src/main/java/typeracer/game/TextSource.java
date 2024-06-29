c77da63777c3c15b4368e6fb452edd965247bd37
Eric Gulbins <gulbinse@cip.ifi.lmu.de>
2024-06-29 11:38:50 +0200

    Remove return of currentText in TextSource setters
    This avoids accidentally not setting the correct text, or missing an update of the current text.

--- game/src/main/java/typeracer/game/TextSource.java
+++ game/src/main/java/typeracer/game/TextSource.java
@@ -4,12 +4,25 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.nio.charset.StandardCharsets;
 
 /** Provides a text to use as prompt for the game. */
 public class TextSource {
   private static final String TEXT_SOURCE_FOLDER = "text_sources";
   private static final String DEFAULT_TEXT_FILE = "bee_movie.txt";
+  private static final String DEFAULT_TEXT_FILE_PATH =
+      TEXT_SOURCE_FOLDER + File.separator + DEFAULT_TEXT_FILE;
+  private static final String DEFAULT_TEXT =
+      """
+          According to all known laws of aviation, there is no way a bee should be able to fly.
+          Its wings are too small to get its fat little body off the ground.
+          The bee, of course, flies anyway because bees don't care what humans think is impossible.
+          Yellow, black. Yellow, black. Yellow, black. Yellow, black.
+          Ooh, black and yellow!
+          Let's shake it up a little.
+          """;
 
   private String currentText;
 
@@ -23,26 +36,17 @@ public class TextSource {
    * @deprecated will be replaced by a method returning a randomly selected text
    */
   @Deprecated
-  public String setDefaultText() {
-    currentText =
-        "According to all known laws of aviation, there is no way a bee should be able to fly.\n"
-            + "Its wings are too small to get its fat little body off the ground.\n"
-            + "The bee, of course, flies anyway because bees don't care what humans think"
-            + "is impossible.\n"
-            + "Yellow, black. Yellow, black. Yellow, black. Yellow, black.\n"
-            + "Ooh, black and yellow!\n"
-            + "Let's shake it up a little.";
-    return currentText;
+  public void setDefaultText() {
+    currentText = DEFAULT_TEXT;
   }
 
   /**
-   * Returns the text in the given file.
+   * Sets the text in the given file as current text.
    *
    * @param file the file to read
-   * @return the text in the given file
    * @throws IOException when exceptions with reading the file occur
    */
-  public String getTextFromFile(File file) throws IOException {
+  public void setTextFromFile(File file) throws IOException {
     try (BufferedReader reader = new BufferedReader(new FileReader(file, StandardCharsets.UTF_8))) {
       String line = reader.readLine();
 
@@ -52,7 +56,6 @@ public class TextSource {
         line = reader.readLine();
       }
       currentText = textBuilder.toString();
-      return currentText;
     }
   }
 
