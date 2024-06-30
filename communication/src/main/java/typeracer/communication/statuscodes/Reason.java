2415d84a60af748304ef8c23c167ade7f6388ef6
Adrian Moritz <ad.moritz@campus.lmu.de>
2024-06-30 02:12:07 +0200

    Add initial status codes

---
+++ communication/src/main/java/typeracer/communication/statuscodes/Reason.java
@@ -0,0 +1,32 @@
+package typeracer.communication.statuscodes;
+
+/** Possible reason for a denied client request. */
+public enum Reason {
+  /** Represents that a player is not allowed to connect. */
+  HANDSHAKE_BANNED("You are currently banned."),
+  /** Represents that the specified session does not exist. */
+  SESSION_NOT_FOUND("This session does not exist."),
+  /** Represents that the game has already started. */
+  SESSION_GAME_ALREADY_STARTED("The game has already started."),
+  /** Represents that the session is already full. */
+  SESSION_FULL("This session is full."),
+  /** Represents that the client does not have permission to create a session. */
+  SESSION_CREATION_NO_PERMISSION("You do not have permission to create a session."),
+  /** Represents that a undefined or unknown error occurred. */
+  UNKNOWN("An unexpected error occurred.");
+
+  private final String reason;
+
+  Reason(String reason) {
+    this.reason = reason;
+  }
+
+  /**
+   * Returns the reason for a denied client request.
+   *
+   * @return the reason for a denied client request
+   */
+  public String getReason() {
+    return reason;
+  }
+}
