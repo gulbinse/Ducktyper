ce31e97decf11dceda7a2ea8ff2316fb59bba63f
Adrian Moritz <ad.moritz@campus.lmu.de>
2024-06-30 19:31:22 +0200

    Add CreateSessionRequestHandler

---
+++ server/src/main/java/typeracer/server/message/handlers/CreateSessionRequestHandler.java
@@ -0,0 +1,45 @@
+package typeracer.server.message.handlers;
+
+import typeracer.communication.messages.Message;
+import typeracer.communication.messages.client.CreateSessionRequest;
+import typeracer.communication.messages.server.CreateSessionResponse;
+import typeracer.communication.statuscodes.Reason;
+import typeracer.server.connection.ConnectionManager;
+import typeracer.server.message.MessageHandler;
+import typeracer.server.session.SessionManager;
+
+/**
+ * Handles CreateSessionRequest messages in a chain of responsibility pattern. If the message is not
+ * of the specified type, it will be passed to the next handler in the chain, if any.
+ */
+public class CreateSessionRequestHandler implements MessageHandler {
+
+  private final MessageHandler nextHandler;
+
+  /** The default constructor of this class. */
+  public CreateSessionRequestHandler() {
+    this.nextHandler = null;
+  }
+
+  private CreateSessionRequestHandler(MessageHandler nextHandler) {
+    this.nextHandler = nextHandler;
+  }
+
+  @Override
+  public void handleMessage(Message message, int clientId) {
+    if (message instanceof CreateSessionRequest createSessionRequest) {
+      SessionManager sessionManager = new SessionManager();
+      int id = sessionManager.createNewSession();
+      Reason reason = id > 0 ? null : Reason.SESSION_CREATE_NO_PERMISSION;
+      ConnectionManager connectionManager = new ConnectionManager();
+      connectionManager.sendMessage(new CreateSessionResponse(reason, id), clientId);
+    } else if (nextHandler != null) {
+      nextHandler.handleMessage(message, clientId);
+    }
+  }
+
+  @Override
+  public CreateSessionRequestHandler setNext(MessageHandler handler) {
+    return new CreateSessionRequestHandler(handler);
+  }
+}
