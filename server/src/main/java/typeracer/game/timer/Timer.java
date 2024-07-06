177d9e0c6a7729e0c4a3fe4b4a97165215e794b1
Adrian Moritz <ad.moritz@campus.lmu.de>
2024-07-06 17:18:38 +0200

    Add missing javadoc

--- server/src/main/java/typeracer/game/timer/Timer.java
+++ server/src/main/java/typeracer/game/timer/Timer.java
@@ -6,6 +6,9 @@ public abstract class Timer {
   /** Returns whether the timer is running. */
   protected boolean isRunning;
 
+  /** The default constructor of this class. */
+  public Timer() {}
+
   /** Starts the timer. */
   public abstract void start();
 
