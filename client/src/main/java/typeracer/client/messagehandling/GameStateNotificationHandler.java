486a87262cb08d06486f96b33933c3ef9b6d4cf9
Adrian Moritz <ad.moritz@campus.lmu.de>
2024-07-13 15:16:23 +0200

    Fix formatting

--- client/src/main/java/typeracer/client/messagehandling/GameStateNotificationHandler.java
+++ client/src/main/java/typeracer/client/messagehandling/GameStateNotificationHandler.java
@@ -30,21 +30,20 @@ public class GameStateNotificationHandler implements MessageHandler {
    */
   @Override
   public void handleMessage(Message message) {
-    try {
-      if (message instanceof GameStateNotification gameStateNotification) {
-        switch (gameStateNotification.getGameStatus()) {
-          case RUNNING:
-            viewController.startNewGame();
-            break;
-          case FINISHED:
-            // ends the game
-            viewController.endGame();
-            break;
-          case WAITING_FOR_PLAYERS:
-            System.out.println("Game is waiting for players.");
-          default:
-            break;
-        }
+    if (message instanceof GameStateNotification gameStateNotification) {
+      switch (gameStateNotification.getGameStatus()) {
+        case RUNNING:
+          viewController.startNewGame();
+          break;
+        case FINISHED:
+          // ends the game
+          viewController.endGame();
+          break;
+        case WAITING_FOR_PLAYERS:
+          System.out.println("Game is waiting for players.");
+        default:
+          break;
+      }
 
     } else if (nextHandler != null) {
       nextHandler.handleMessage(message);
